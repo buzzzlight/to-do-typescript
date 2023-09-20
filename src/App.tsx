61deaa5b3b8cd8a642a6942d04386e70e86cea76
김수빈 <pomodoro5292@gmail.com>
2023-09-21 01:31:13 +0900

    js -> tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+// 자바스크립트 -> 타입스크립트 마이그레이션
+// 1. 타입선언 패키지 설치
+// 2. tsconfig.json
+// 3. 모든 js파일 -> jsx파일
+// 4. 하나씩 tsx형식으로 만들고 오류 해결
